424a944520d3cd86b7bd4fe8871bca47460905aa
indian_potato@naver.com <indian_potato@naver.com>
2023-03-28 18:53:34 +0900

    Learn: Number Guessing Game Using Javascript

---
+++ js/generator.js
@@ -0,0 +1,45 @@
+const randomForm = document.querySelector("#random-form");
+const randomNumber = document.querySelector("#random-form #random-number");
+const randomGuess = document.querySelector("#random-form #random-guess");
+const choseNumber = document.querySelector("#random-form #chose-number");
+let youWin = document.querySelector("#random-form #you-win");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USER_CHOSE = "userChose";
+
+function onRandomSubmit(event) {
+  event.preventDefault();
+  const value = randomGuess.value;
+  if (value === "" || value < 0) {
+    alert("Please enter a valid number");
+  } else {
+    localStorage.setItem(USER_CHOSE, randomNumber.value);
+    paintNumber(value);
+  }
+}
+
+function paintNumber(value) {
+  const machineChose = Math.floor(Math.random() * randomNumber.value);
+  choseNumber.innerText = `You chose: ${value}, the machine chose: ${machineChose}`;
+  choseNumber.classList.remove(HIDDEN_CLASSNAME);
+  youWin.classList.remove(HIDDEN_CLASSNAME);
+  win(value, machineChose);
+}
+const userChoseSaved = localStorage.getItem(USER_CHOSE);
+
+function win(value, machineChose) {
+  console.log(value, machineChose);
+  if (value == machineChose) {
+    youWin.innerText = "You Win!";
+  } else {
+    youWin.innerText = "You lost!";
+  }
+}
+
+if (userChoseSaved === null) {
+  choseNumber.classList.add(HIDDEN_CLASSNAME);
+  youWin.classList.add(HIDDEN_CLASSNAME);
+  randomForm.addEventListener("submit", onRandomSubmit);
+} else {
+  paintNumber(userChoseSaved);
+}
